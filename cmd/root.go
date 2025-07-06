42e6186ca35e5f7b487f1f89a607f3ca896f5613
arfadmuzali <arfadmuzali258@gmail.com>
2025-07-06 16:08:32 +0700

    docs: change default Short and Long documentation root

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "todo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A simple CLI app to manage your todo tasks",
+	Long: `Todo is a command-line application to help you manage your tasks efficiently.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+You can add, list, update, delete, and toggle your todo items.
+All data is saved locally in a JSON file (by default: ~/.config/todo/todos.json).
+
+Examples:
+
+  todo add "Learn Golang"
+  todo list
+  todo update 1 "Finish Cobra integration"
+  todo toggle 1
+  todo delete 1 2
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +51,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
